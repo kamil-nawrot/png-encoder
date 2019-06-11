88ed0a6f7eb89a68e6723a33cac8245116c12449
Kamil Nawrot <kamilnawrot98@gmail.com>
2019-06-11 10:52:54 +0200

    image encryption

--- png-decoder.py
+++ png-decoder.py
@@ -1,155 +1,257 @@
 import os
 import zlib
-import cmath
 import numpy as np
 import matplotlib.pyplot as plt
+import png
+import rsa
 
-fileName = "./img/image0.png"
-imageFile = open(fileName, "rb")
-fileSize = os.path.getsize(fileName)
-print("FILE SIZE (in bytes): " + str(fileSize))
 
-def check_if_png(signature):
-    if type(signature) is bytes and signature == b'\x89PNG\r\n\x1a\n':
-        return True
-    elif type(signature) is str and signature == '89504e470d0a1a0a':
-        return True
-    elif type(signature) is int and signature == 9894494448401390090:
-        return True
-    else:
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+class ImageFile:
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.desc = open(self.filename, mode)
+        self.fileSize = os.path.getsize(self.filename)
+        self.imageProperties = {}
+        self.content = None
+        self.encodedContent = None
+        self.compressedContent = None
+        self.contentSize = None
+        self.signature = self.desc.read(8)
+        self.pixelArray = []
+        self.redChannel = []
+        self.greenChannel = []
+        self.blueChannel = []
+        self.keys = []
+
+    def find_chunk(self, chunk_type):
+        self.desc.seek(0, 0)
+        while self.desc.tell() < self.fileSize:
+            if self.desc.read(1).find(bytes(chunk_type[0], 'utf-8')) != -1:
+                if self.desc.read(3).find(bytes(chunk_type[1:4], 'utf-8')) != -1:
+                    self.desc.seek(-8, 1)
+                    chunk_length = int.from_bytes(self.desc.read(4), byteorder="big")
+                    print(
+                        chunk_type + " found (length: " + str(chunk_length) + ") at position " + str(self.desc.tell()))
+                    self.desc.read(4)
+                    return chunk_length
+        print(chunk_type + ": not found")
         return False
 
-
-def find_chunk(chunk_type):
-    imageFile.seek(0, 0)
-    while imageFile.tell() < fileSize:
-        if imageFile.read(1).find(bytes(chunk_type[0], 'utf-8')) != -1:
-            if imageFile.read(3).find(bytes(chunk_type[1:4], 'utf-8')) != -1:
-                imageFile.seek(-8, 1)
-                chunk_length = int.from_bytes(imageFile.read(4), byteorder="big")
-                print(chunk_type + " found (length: " + str(chunk_length) + ") at position " + str(imageFile.tell()))
-                imageFile.read(4)
-                return chunk_length
-    print(chunk_type + ": not found")
-    return False
-
-
-# file signature:
-fileSig = imageFile.read(8)
-intBytes = int.from_bytes(fileSig, byteorder="big")
-
-# file header:
-if check_if_png(fileSig):
-    
-    find_chunk("IHDR")
-    
-    width = int.from_bytes(imageFile.read(4), byteorder="big")
-    height = int.from_bytes(imageFile.read(4), byteorder="big")
-    print("IMAGE DIMENSIONS:       " + str(width) + "x" + str(height))
-    bitDepth = int.from_bytes(imageFile.read(1), byteorder="big")
-    print("BIT DEPTH:              " + str(bitDepth))
-
-
-    def color_type_switch (color):
-        switcher = {
-            0: "1 - gray scale",
-            2: "3 - true color (RGB)",
-            3: "1 - indexed",
-            4: "2 - gray scale with alpha",
-            6: "4 - RGB with alpha"
-        }
-        return switcher.get(color, "invalid color type")
-
-
-    def chunks(l, n):
-        for i in range(0, len(l), n):
-            yield l[i:i + n]
-
-    colorType = int.from_bytes(imageFile.read(1), byteorder="big")
-    print("COLOR TYPE:             " + color_type_switch(colorType))
-    compressionMethod = int.from_bytes(imageFile.read(1), byteorder="big")
-    if compressionMethod == 0:
-        print("COMPRESSION METHOD:     " + str(compressionMethod) + " - deflate")
-    else:
-        print("COMPRESSION METHOD:     " + str(compressionMethod) + " - unknown method")
-    filterMethod = int.from_bytes(imageFile.read(1), byteorder="big")
-    if filterMethod == 0:
-        print("FILTER METHOD:          " + str(filterMethod) + " - default")
-    else:
-        print("FILTER METHOD:          " + str(filterMethod) + " - unknown method")
-    interlaceMethod = int.from_bytes(imageFile.read(1), byteorder="big")
-    if interlaceMethod == 0:
-        print("INTERLACE METHOD:       " + str(interlaceMethod) + " - no interlace")
-    else:
-        print("INTERLACE METHOD:       " + str(interlaceMethod) + " - Adam7")
-
-    if find_chunk("tIME"):
-        lastEdit = str(int.from_bytes(imageFile.read(2), byteorder="big"))
-        lastEdit += "/"
-        lastEdit += str(int.from_bytes(imageFile.read(1), byteorder="big")).zfill(2)
-        lastEdit += "/"
-        lastEdit += str(int.from_bytes(imageFile.read(1), byteorder="big")).zfill(2)
-        lastEdit += " "
-        lastEdit += str(int.from_bytes(imageFile.read(1), byteorder="big")).zfill(2)
-        lastEdit += ":"
-        lastEdit += str(int.from_bytes(imageFile.read(1), byteorder="big")).zfill(2)
-        lastEdit += ":"
-        lastEdit += str(int.from_bytes(imageFile.read(1), byteorder="big")).zfill(2)
-        print(lastEdit)
-
-    chunkSize = 0
-    if not find_chunk("iTXt"):
-        if not find_chunk("tEXt"):
-            if not find_chunk("zTXt"):
-                print("no textual data found")
-            else:
-                chunkSize = find_chunk("zTXt")
+    def check_if_png(self):
+        self.desc.seek(0, 0)
+        self.signature = self.desc.read(8)
+        if type(self.signature) is bytes and self.signature == b'\x89PNG\r\n\x1a\n':
+            return True
+        elif type(self.signature) is str and self.signature == '89504e470d0a1a0a':
+            return True
+        elif type(self.signature) is int and self.signature == 9894494448401390090:
+            return True
         else:
-            chunkSize = find_chunk("tEXt")
-    else:
-        chunkSize = find_chunk("iTXt")
+            return False
 
-    if chunkSize > 0:
-        print(imageFile.read(chunkSize).decode('utf-8'))
+    def update_properties(self):
+        self.find_chunk("IHDR")
+        self.imageProperties["width"] = int.from_bytes(self.desc.read(4), byteorder="big")
+        self.imageProperties["height"] = int.from_bytes(self.desc.read(4), byteorder="big")
+        self.imageProperties["bitDepth"] = int.from_bytes(self.desc.read(1), byteorder="big")
+        self.imageProperties["colorType"] = int.from_bytes(self.desc.read(1), byteorder="big")
+        self.imageProperties["compressionMethod"] = int.from_bytes(self.desc.read(1), byteorder="big")
+        self.imageProperties["filterMethod"] = int.from_bytes(self.desc.read(1), byteorder="big")
+        self.imageProperties["interlaceMethod"] = int.from_bytes(self.desc.read(1), byteorder="big")
 
-    find_chunk("hIST")
-    find_chunk("gAMA")
-    find_chunk("cHRM")
+    def display_properties(self):
+        def color_type_switch(color):
+            switcher = {
+                0: "1 - gray scale",
+                2: "3 - true color (RGB)",
+                3: "1 - indexed",
+                4: "2 - gray scale with alpha",
+                6: "4 - RGB with alpha"
+            }
+            return switcher.get(color, "invalid color type")
 
-    chunkSize = find_chunk("IDAT")
-    data = imageFile.read(chunkSize)
-    pixelArray = []
-    print(data)
-    decompressed = zlib.decompress(data)
-    print(decompressed)
-    for index, pixel in enumerate(decompressed):
-        if index % (3*width + 1) is not 0:
-            pixelArray.append(pixel)
+        print("FILE SIZE:              " + str(self.fileSize) + " bytes")
+        print("IMAGE DIMENSIONS:       " + str(self.imageProperties["width"]) + "x" + str(self.imageProperties["height"]))
+        print("BIT DEPTH:              " + str(self.imageProperties["bitDepth"]))
+        print("COLOR TYPE:             " + color_type_switch(self.imageProperties["colorType"]))
+        if self.imageProperties["compressionMethod"] == 0:
+            print("COMPRESSION METHOD:     " + str(self.imageProperties["compressionMethod"]) + " - deflate")
+        else:
+            print("COMPRESSION METHOD:     " + str(self.imageProperties["compressionMethod"]) + " - unknown method")
+        if self.imageProperties["filterMethod"] == 0:
+            print("FILTER METHOD:          " + str(self.imageProperties["filterMethod"]) + " - default")
+        else:
+            print("FILTER METHOD:          " + str(self.imageProperties["filterMethod"]) + " - unknown method")
+        if self.imageProperties["interlaceMethod"] == 0:
+            print("INTERLACE METHOD:       " + str(self.imageProperties["interlaceMethod"]) + " - no interlace")
+        else:
+            print("INTERLACE METHOD:       " + str(self.imageProperties["interlaceMethod"]) + " - Adam7")
 
-    redChannel = []
-    greenChannel = []
-    blueChannel = []
-    pixelArray = list(chunks(pixelArray, 3))
-    print(pixelArray)
-    for pixel in pixelArray:
-        redChannel.append(pixel[0])
-        greenChannel.append(pixel[1])
-        blueChannel.append(pixel[2])
+        chunk_size = 0
+        if not self.find_chunk("iTXt"):
+            if not self.find_chunk("tEXt"):
+                if not self.find_chunk("zTXt"):
+                    print("no textual data found")
+                else:
+                    chunk_size = self.find_chunk("zTXt")
+            else:
+                chunk_size = self.find_chunk("tEXt")
+        else:
+            chunk_size = self.find_chunk("iTXt")
 
-    red_spectrum = np.fft.fft(redChannel[:1024])
-    red_spectrum_amp = np.abs(red_spectrum)
-    plt.plot(red_spectrum_amp)
-    plt.show()
+        if chunk_size > 0:
+            print(self.desc.read(chunk_size).decode('utf-8'))
 
-    green_spectrum = np.fft.fft(greenChannel[:1024])
-    green_spectrum_amp = np.abs(green_spectrum)
-    plt.plot(green_spectrum_amp)
-    plt.show()
+    def read_content(self):
+        chunk_size = self.find_chunk("IDAT")
+        self.compressedContent = self.desc.read(chunk_size)
 
-    blue_spectrum = np.fft.fft(blueChannel[:1024])
-    blue_spectrum_amp = np.abs(blue_spectrum)
-    plt.plot(blue_spectrum_amp)
-    plt.show()
+    def add_compressed_content(self, content):
+        self.compressedContent = content
+
+    def decompress(self):
+        self.content = zlib.decompress(self.compressedContent)
+        self.contentSize = len(self.content)
+
+    def create_pixel_array(self):
+        for index, pixel in enumerate(self.content):
+            if index % (3*self.imageProperties["width"] + 1) is not 0:
+                self.pixelArray.append(pixel)
+
+        self.redChannel = []
+        self.greenChannel = []
+        self.blueChannel = []
+        self.pixelArray = list(chunks(self.pixelArray, 3))
+        for pixel in self.pixelArray:
+            self.redChannel.append(pixel[0])
+            self.greenChannel.append(pixel[1])
+            self.blueChannel.append(pixel[2])
+
+    def compute_spectrum(self, color):
+        spectrum = None
+        if color == "red":
+            spectrum = np.fft.fft(self.redChannel[:1024])
+        elif color == "green":
+            spectrum = np.fft.fft(self.greenChannel[:1024])
+        elif color == "blue":
+            spectrum = np.fft.fft(self.blueChannel[:1024])
+        spectrum_amp = np.abs(spectrum)
+        plt.plot(spectrum_amp)
+        plt.show()
+
+    def encode_with_rsa(self):
+        image_data = b""
+        for index, pixel in enumerate(self.content):
+            if index % (3 * self.imageProperties["width"] + 1) is not 0:
+                image_data += pixel.to_bytes(1, byteorder="big")
+
+        [n, e, d] = rsa.generate_key_pairs(256)
+        self.keys = [n, e, d]
+        encoded_data = b""
+        encoded_block = b""
+
+        for x in range(1, (self.contentSize // 4) + 1):
+            print(encoded_block)
+            encoded_block = b""
+            for byte in image_data[32*(x-1):32*x]:
+                encoded_block += byte.to_bytes(1, byteorder="big")
+            encoded_data += rsa.rsa_encode(n, e, int.from_bytes(encoded_block, byteorder="big")).to_bytes(32, byteorder="big")
+
+        encodedArray = []
+        for index, pixel in enumerate(encoded_data):
+            encodedArray.append(pixel)
+
+        encodedArray = list(chunks(encodedArray, self.imageProperties["width"]*3))
+        self.encodedContent = encoded_data
+        print("Encoded:", self.encodedContent)
+
+        png.from_array(encodedArray[:self.imageProperties["width"]][:self.imageProperties["height"]], 'RGB')\
+            .save('./img/result.png')
+
+    def decode_with_rsa(self):
+        decoded_data = b""
+        decoded_block = b""
+
+        for x in range(1, (self.contentSize // 32) + 2):
+            decoded_block = b""
+            for byte in self.encodedContent[32*x:32*(x+1)]:
+                print(byte)
+                decoded_block += byte.to_bytes(1, byteorder="big")
+            decoded_data += rsa.rsa_decode(self.keys[0], self.keys[2], int.from_bytes(decoded_block, byteorder="big")).to_bytes(32, byteorder="big")
+            print("Decoded:", decoded_data)
+
+        print("Decoded:", decoded_data)
+
+        decodedArray = []
+        for index, pixel in enumerate(decoded_data):
+            decodedArray.append(pixel)
+
+        decodedArray = list(chunks(decodedArray, self.imageProperties["width"]*3))
+        print(decodedArray)
+
+        png.from_array(decodedArray[:self.imageProperties["width"]][:self.imageProperties["height"]], 'RGB') \
+            .save('./img/result-decoded.png')
+
+##########################################################
+
+
+originalImage = ImageFile("./img/image50x50.png", "rb")
+if originalImage.check_if_png():
+    originalImage.update_properties()
+    originalImage.read_content()
+    originalImage.decompress()
+    originalImage.create_pixel_array()
+    originalImage.display_properties()
+    # originalImage.compute_spectrum("red")
+    # originalImage.compute_spectrum("green")
+    # originalImage.compute_spectrum("blue")
+
+
+    originalImage.encode_with_rsa()
+    originalImage.decode_with_rsa()
+
+    # flat_pixel_array = []
+    # for sublist in originalImage.pixelArray:
+    #     for item in sublist:
+    #         flat_pixel_array.append(item)
+    #
+    # final = b""
+    # for byte in flat_pixel_array:
+    #     final += byte.to_bytes(1, byteorder="big")
+    #
+    # flat_pixel_array = list(chunks(flat_pixel_array, originalImage.imageProperties["width"]**2))
+    # # png.from_array(flat_pixel_array, 'RGB').save('./img/result.png')
+    #
+    # image_data = int.from_bytes(final, byteorder="big")
+    #
+    # [n, e, d] = rsa.generate_key_pairs(256)
+    #
+    # encoded_image = rsa.rsa_encode(n, e, image_data)
+    # print(encoded_image.to_bytes(32, byteorder="big"))
+    #
+    # enc = encoded_image.to_bytes(36, byteorder="big")
+    #
+    # print("enc", enc)
+    #
+    # flat_final = []
+    # for pixel in enumerate(enc):
+    #     print(pixel)
+    #     flat_final.append(pixel[1])
+    #
+    # flat_final = list(chunks(flat_final, originalImage.imageProperties["width"]**2))
+    # print(flat_final)
+    #
+    # png.from_array(flat_final, 'RGB').save('./img/result.png')
+    #
+    #
+    #
+    # decoded_image = rsa.rsa_decode(n, d, encoded_image)
+    # print(decoded_image.to_bytes(32, byteorder="big"))
 
 else:
-    print("wrong file format")
+    print("Wrong file format")
--- rsa.py
+++ rsa.py
@@ -13,57 +13,105 @@ def gcd(a, b):
     return a
 
 
-def is_prime(n, k):
-    if n == 2:
-        return True
-
-    if n % 2 == 0:
-        return False
-
-    r, s = 0, n - 1
+def rabin_miller(num):
+    s = num - 1
+    t = 0
     while s % 2 == 0:
-        r += 1
-        s //= 2
-    for _ in range(k):
-        a = random.randrange(2, n - 1)
-        x = pow(a, s, n)
-        if x == 1 or x == n - 1:
-            continue
-        for _ in range(r - 1):
-            x = pow(x, 2, n)
-            if x == n - 1:
-                break
-        else:
-            return False
+        s = s // 2
+        t += 1
+
+    for trials in range(40):
+        a = random.randrange(2, num - 1)
+        v = pow(a, s, num)
+        if v != 1:
+            i = 0
+            while v != (num - 1):
+                if i == t - 1:
+                    return False
+                else:
+                    i = i + 1
+                    v = (v ** 2) % num
     return True
 
 
+def is_prime(num):
+    if num < 2:
+        return False
+    low_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
+                  103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
+                  211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317,
+                  331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443,
+                  449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577,
+                  587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701,
+                  709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839,
+                  853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983,
+                  991, 997]
+    if num in low_primes:
+        return True
+    for prime in low_primes:
+        if num % prime == 0:
+            return False
+    return rabin_miller(num)
+
+
+def mod(num, a):
+    res = 0
+    for i in range(0, len(num)):
+        res = (res * 10 + int(num[i])) % a
+    return res
+
+
 def generate_key_pairs(key_length):
-    p = random.randrange(1 << int(0.5*key_length-1), 1 << int(0.5*key_length))
-    while is_prime(p, 40) is not True:
-        p = random.randrange(1 << int(0.5*key_length-1), 1 << int(0.5*key_length))
 
-    e = 65537
-
-    q = random.randrange(1 << int(0.5*key_length-1), 1 << int(0.5*key_length))
-    while is_prime(q, 40) is not True or q == p or gcd(e, phi) != 1:
-        q = random.randrange(1 << int(0.5*key_length-1), 1 << int(0.5*key_length))
-        if p < q:
-            p, q = q, p
-
-            n = p*q
-            phi = (p-1)*(q-1)
-
-    for k in range(0, e):
-        d = (1 + k*phi) / e
-        if d.is_integer():
-            d = int(d)
+    while True:
+        p = random.randrange(1 << int(0.5 * key_length - 1), 1 << int(0.5 * key_length))
+        if is_prime(p):
             break
 
-    print("PUBLIC KEY: ", n, e)
-    print("PRIVATE KEY: ", n, d)
+    e = 65537
+    phi = 0
+
+    while True:
+        q = random.randrange(1 << int(0.5*key_length-1), 1 << int(0.5*key_length))
+        n = p*q
+        phi = (p-1)*(q-1)
+        if is_prime(q) and q != p and gcd(e, phi) == 1:
+            break
+
+    if q > p:
+        p, q = q, p
+
+    # print("FINAL P:", p)
+    # print("FINAL Q:", q)
+
+    mod_inv = lambda a, b, s=1, t=0, N=0: (b < 2 and t % N or mod_inv(b, a % b, t, s - a // b * t, N or b), -1)[b < 1]
+
+    d = mod_inv(e, phi)
+
+    # print("PUBLIC KEY: ", hex(n), hex(e))
+    # print("PRIVATE KEY: ", hex(n), hex(d))
+    # print("P: ", p, " Q: ", q, " N: ", n, "D: ", d)
 
     return [n, e, d]
 
 
-generate_key_pairs(64)
+def rsa_encode(n, e, data):
+    encoded_data = pow(data, e, n)
+    return encoded_data
+
+
+def rsa_decode(n, d, data):
+    decoded_data = pow(data, d, n)
+    return decoded_data
+
+
+[key, public_key, private_key] = generate_key_pairs(256)
+m = b'\x00\xff\x00\x00\x00\xff\x00\x00\x00\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+m = int.from_bytes(m, byteorder="big")
+
+encoded = rsa_encode(key, public_key, m)
+print(encoded.to_bytes(32, byteorder="big"))
+decoded = rsa_decode(key, private_key, encoded)
+print(decoded.to_bytes(32, byteorder="big"))
+
+
